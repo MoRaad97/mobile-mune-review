c98188d1d7c433ec15ca4519d73f2c2f44a8b027
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-10-20 12:13:52 +0300

    Fix Linter errors

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const nav_open = document.querySelector('.menu-bars');
-const nav_close = document.querySelector('.close-btn-wrapper');
-const menu_element = document.querySelector('.menu');
-const links_action = menu_element.querySelectorAll('a');
+const navOpen = document.querySelector('.menu-bars');
+const navClose = document.querySelector('.close-btn-wrapper');
+const menuElement = document.querySelector('.menu');
+const linksAction = menuElement.querySelectorAll('a');
 
-nav_open.addEventListener('click', () => {
-  menu_element.classList.remove('hidden');
+navOpen.addEventListener('click', () => {
+  menuElement.classList.remove('hidden');
   document.querySelector('body').classList.add('overflow-hidden');
 });
 
-nav_close.addEventListener('click', () => {
-  menu_element.classList.add('none');
+navClose.addEventListener('click', () => {
+  menuElement.classList.add('none');
   document.querySelector('body').classList.remove('overflow-hidden');
 });
 
-links_action.forEach((node) => {
+linksAction.forEach((node) => {
   node.onclick = () => {
-    menu_element.classList.add('hidden');
+    menuElement.classList.add('hidden');
     document.querySelector('body').classList.remove('overflow-hidden');
   };
 });
